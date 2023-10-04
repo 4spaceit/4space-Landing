873d5e90c26ce67ef21c02ae18c60666f04cface
Amr Fateem <development@digializer.com>
2023-10-04 19:24:26 +0300

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -22,6 +22,44 @@ export default function Testimonial() {
             </div>
           </div>
         </div>
+        <div className=" column is-one-quarter testimonial-box">
+          <p className="has-text-black">
+            <span className="font-bold has-text-white is-size-3"> “ </span>
+            To get social media testimonials like these, keep your customers
+            engaged with your social media accounts by posting regularly
+            yourself
+            <span className="font-bold has-text-white is-size-3"> ” </span>
+          </p>
+          <div className="is-flex is-align-items-center is-justify-content-start mt-4">
+            <div className="is-flex is-flex-direction-column ml-2 is-align-content-space-between">
+              <span className="font-semibold has-text-weight-bold has-text-white">
+                Reviewee
+              </span>
+              <span className="is-size-7 is-flex has-text-black is-align-items-center">
+                Position
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className=" column is-one-quarter testimonial-box">
+          <p className="has-text-black">
+            <span className="font-bold has-text-white is-size-3"> “ </span>
+            To get social media testimonials like these, keep your customers
+            engaged with your social media accounts by posting regularly
+            yourself
+            <span className="font-bold has-text-white is-size-3"> ” </span>
+          </p>
+          <div className="is-flex is-align-items-center is-justify-content-start mt-4">
+            <div className="is-flex is-flex-direction-column ml-2 is-align-content-space-between">
+              <span className="font-semibold has-text-weight-bold has-text-white">
+                Reviewee
+              </span>
+              <span className="is-size-7 is-flex has-text-black is-align-items-center">
+                Position
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
