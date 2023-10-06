75a2e84250d21987f06736e4bcdef0843c9e11ac
Amr Fateem <development@digializer.com>
2023-10-06 15:17:56 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,13 +16,14 @@ export default function Header() {
         >
           <div className="navbar-brand">
             <Logo />
-            <button onClick={() => setOpen((v) => !v)} className={`navbar-burger burger link-touch ${ open ? "is-active" : "" }`} aria-label="Menu" > <Menu /> </button>
+            <button onClick={() => setOpen((v) => !v)} className={`navbar-burger burger link-touch ${open ? "is-active" : ""}`} aria-label="Menu" > <Menu /> </button>
           </div>
           <div className={`navbar-menu link-touch ${open ? "is-active" : ""}`}>
             <div className="navbar-end">
               <Link rel="nofollow" to="projects" spy={true} offset={-50} smooth={true} className="navbar-item" onClick={() => setOpen(false)} > Projects </Link>
               <Link rel="nofollow" to="about" offset={-50} spy={true} smooth={true} className="navbar-item" onClick={() => setOpen(false)} > About </Link>
               <Link rel="nofollow" to="awards" spy={true} smooth={true} className="navbar-item" onClick={() => setOpen(false)} > Awards </Link>
+              <Link rel="nofollow" to="our-clients" spy={true} smooth={true} className="navbar-item" onClick={() => setOpen(false)} > Our Clients </Link>
               <Link rel="nofollow" to="testimonial" spy={true} smooth={true} className="navbar-item" onClick={() => setOpen(false)} > Testimonial </Link>
               <button className="button is-primary is-outlined is-hidden-touch " id="formButton" >
                 <Link rel="nofollow" spy={true} smooth={true} onClick={() => { document.getElementById("modal").classList.add("is-active"); }} > PROJECT INQUIRY </Link>
