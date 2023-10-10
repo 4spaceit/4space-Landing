b8e7322d0714aa084e9df95e3bac8f8f86640520
Amr Fateem <development@digializer.com>
2023-10-10 14:31:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -97,14 +97,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/en/commercial" element={<App data={dataCommercialEn} video={CommercialVideo} bg={"https://4spacewp.com/wp-content/uploads/2021/02/4space-office-01-2.jpg"} />} />
         <Route path="/en/branding" element={<App data={dataBrandingEn} video={BrandingVideo} bg={"./images/first.jpg"} />} />
 
-        <Route path="/ar/resturant" element={<LandingAr data={dataRestaurantAr} video={ResturantVideo} bg={"./images/first.jpg"} />} />
+        <Route path="/ar/restaurant" element={<LandingAr data={dataRestaurantAr} video={ResturantVideo} bg={"./images/first.jpg"} />} />
         <Route path="/ar/retail" element={<LandingAr data={dataRetailAr} video={RetailVideo} bg={"https://4spacewp.com/wp-content/uploads/2018/03/VR-Park-5-scaled.jpg"} />} />
         <Route path="/ar/residential" element={<LandingAr data={dataResidentialAr} video={ResidentialVideo} bg={"https://4spacewp.com/wp-content/uploads/2019/05/Residential-Villa-in-Saudi-Arabia-by-4SPACE-001-scaled.jpg"} />} />
         <Route path="/ar/commercial" element={<LandingAr data={dataCommercialAr} video={CommercialVideo} bg={"https://4spacewp.com/wp-content/uploads/2021/02/4space-office-01-2.jpg"} />} />
         <Route path="/ar/branding" element={<LandingAr data={dataBrandingAr} video={BrandingVideo} bg={"./images/first.jpg"} />} />
 
 
-        <Route path="/ru/resturant" element={<LandingRu data={dataRestaurantRu} video={ResturantVideo} videoM={ResturantVideoM} bg={"https://4spacewp.com/wp-content/uploads/2017/10/KHOFO-Dubai-Restaurant-06-scaled.jpg"} />} />
+        <Route path="/ru/restaurant" element={<LandingRu data={dataRestaurantRu} video={ResturantVideo} videoM={ResturantVideoM} bg={"https://4spacewp.com/wp-content/uploads/2017/10/KHOFO-Dubai-Restaurant-06-scaled.jpg"} />} />
         <Route path="/ru/retail" element={<LandingRu data={dataRetailRu} video={RetailVideo} bg={"https://4spacewp.com/wp-content/uploads/2018/03/VR-Park-5-scaled.jpg"} />} />
         <Route path="/ru/residential" element={<LandingRu data={dataResidentialRu} video={ResidentialVideo} bg={"https://4spacewp.com/wp-content/uploads/2019/05/Residential-Villa-in-Saudi-Arabia-by-4SPACE-001-scaled.jpg"} />} />
         <Route path="/ru/commercial" element={<LandingRu data={dataCommercialRu}  video={CommercialVideo} bg={"https://4spacewp.com/wp-content/uploads/2021/02/4space-office-01-2.jpg"} />} />
